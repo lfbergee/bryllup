8c97e6a0fd62a24e43d1974bd10050f85af2c4df
lfbergee <lfberge@gmail.com>
2023-11-11 15:40:36 +0100

    save the date

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,26 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Cormorant } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Cormorant } from "next/font/google";
+import Image from "next/image";
 
-const lora = Cormorant({ subsets: ['latin'] })
+const lora = Cormorant({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Monica & Leiv Fredriks bryllup',
-  description: '',
-}
+  title: "Monica & Leiv Fredriks bryllup",
+  description: "",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body data-theme="fantasy" className={lora.className}>{children}
+      <body data-theme="fantasy" className={lora.className}>
+        <Image fill src="/rings.jpg" className="blur object-cover" alt="" />
+        {children}
       </body>
     </html>
-  )
+  );
 }
